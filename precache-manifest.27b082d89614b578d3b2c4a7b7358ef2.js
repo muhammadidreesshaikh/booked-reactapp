777f698702b8b7c5941adf4ec59e51a5d47000f3
Awais Shaikh <awaisshaikh.official@gmail.com>
2021-02-13 14:48:51 +0500

    Updates

---
+++ precache-manifest.27b082d89614b578d3b2c4a7b7358ef2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691055c9dc8234cbd89b8e28f9a2ca1a",
+    "url": "/booked-reactapp/index.html"
+  },
+  {
+    "revision": "2c094898dc681bc8fa9d",
+    "url": "/booked-reactapp/static/css/2.25b11ed4.chunk.css"
+  },
+  {
+    "revision": "a24595b208f9c1a62a7c",
+    "url": "/booked-reactapp/static/css/main.0bd61a89.chunk.css"
+  },
+  {
+    "revision": "2c094898dc681bc8fa9d",
+    "url": "/booked-reactapp/static/js/2.7bed7b7b.chunk.js"
+  },
+  {
+    "revision": "d99c517a1b251984137db278335a4085",
+    "url": "/booked-reactapp/static/js/2.7bed7b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24595b208f9c1a62a7c",
+    "url": "/booked-reactapp/static/js/main.beadf643.chunk.js"
+  },
+  {
+    "revision": "48d25cf438db1f05091d",
+    "url": "/booked-reactapp/static/js/runtime-main.7f4792f4.js"
+  },
+  {
+    "revision": "da1d4afd7a5d8ff793e765980d808885",
+    "url": "/booked-reactapp/static/media/img.da1d4afd.png"
+  },
+  {
+    "revision": "070215c939ca1c20e25ead903fd32e52",
+    "url": "/booked-reactapp/static/media/img1.070215c9.jpg"
+  },
+  {
+    "revision": "2ba3e7bbaade4b30e29af32aea668819",
+    "url": "/booked-reactapp/static/media/img2.2ba3e7bb.jpg"
+  },
+  {
+    "revision": "eacbf936ffe921080a862535ed1d5fd3",
+    "url": "/booked-reactapp/static/media/img3.eacbf936.jpg"
+  },
+  {
+    "revision": "85d94044bbe4c73e181468761909fa0d",
+    "url": "/booked-reactapp/static/media/img4.85d94044.jpg"
+  },
+  {
+    "revision": "14113f352b9f9d230278c86148b60237",
+    "url": "/booked-reactapp/static/media/img5.14113f35.jpg"
+  },
+  {
+    "revision": "c0450797e2e35b8b73b49fa34cec6bc4",
+    "url": "/booked-reactapp/static/media/img6.c0450797.jpg"
+  }
+]);
