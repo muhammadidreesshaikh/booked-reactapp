b8609630e97bcbf9e07dcd2a0ca161e5c247ab38
Muhammad Idrees <idreesiqbal104@gmail.com>
2021-02-19 23:59:14 +0500

    till now updated work

--- src/components/Booking/BookingList.js
+++ src/components/Booking/BookingList.js
@@ -176,7 +176,7 @@ class BookingList extends React.Component {
                             </div>
                         </div>
 
-                        <div className="form pt-3">
+                        <div className="form pt-4">
                             <div className="container">
 
                                 <div className="col-md-12">
@@ -184,17 +184,48 @@ class BookingList extends React.Component {
                                         <i class="fas fa-calendar-week"></i> <input type="text" placeholder="May 25, 2020"/> <a href="#">Change</a>
                                     </div>
 
-                                    <div className="input mt-3">
+                                    <div className="input mt-2">
                                         <i class="far fa-clock"></i> <input type="text" placeholder="Room Name / 01:00pm - 12:00pm"/> <a href="#">Change</a>
                                     </div>
                                 </div>
 
                                 <div className="col-md-12">
+                                    <div className="name pt-4">
+                                        <input type="text" placeholder="Joe Semth"/> <a href="#">*</a>
+                                    </div>
+
+                                    <div className="name mt-2">
+                                        <input type="text" placeholder="2020 Financials" /> 
+                                    </div>
                                     
+                                    <div className="label pt-2">
+                                        <div className="row">
+                                            <span className="mx-3">?</span> <p>This is some help text for the field above.</p>
+                                        </div>
+                                    </div>
                                 </div>
 
                                 <div className="col-md-12">
-                                    
+                                    <div className="name pt-4">
+                                        <input type="text" placeholder="Metting Description (optional)"/>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-12">
+                                    <div className="metting-needs pt-4">
+                                        <h6>Metting Needs</h6>
+                                        <p className="pt-3">This is a field with a field title and field description. Both of these are optional and will display like this if included.</p>
+
+                                        <div class="form-group">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required/>
+                                                <label class="form-check-label" for="invalidCheck2">
+                                                    Agree to terms and conditions
+                                                </label>
+                                            </div>
+                                        </div>
+
+                                    </div>
                                 </div>
 
                             </div>
