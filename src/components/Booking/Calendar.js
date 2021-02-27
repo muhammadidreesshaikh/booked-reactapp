6290a436dc80aa8656eb701849e1cf6b8e1a58fd
Muhammad Idrees <idreesiqbal104@gmail.com>
2021-02-27 23:56:43 +0500

    calendar events

--- src/components/Booking/Calendar.js
+++ src/components/Booking/Calendar.js
@@ -11,7 +11,61 @@ class Calendar extends React.Component {
 
         this.state = {
             data: [],
-            loading: false
+            loading: false,
+            events: [
+                {
+                    title: 'All Day Event',
+                    start: '2021-02-01'
+                  },
+                  {
+                    title: 'Long Event',
+                    start: '2021-02-07',
+                    end: '2021-02-10',
+                    color: 'purple' // override!
+                  },
+                  {
+                    groupId: '999',
+                    title: 'Repeating Event',
+                    start: '2021-02-09T16:00:00'
+                  },
+                  {
+                    groupId: '999',
+                    title: 'Repeating Event',
+                    start: '2021-02-16T16:00:00'
+                  },
+                  {
+                    title: 'Conference',
+                    start: '2021-02-11',
+                    end: '2021-02-13',
+                    color: 'purple' // override!
+                  },
+                  {
+                    title: 'Meeting',
+                    start: '2021-02-12T10:30:00',
+                    end: '2021-02-12T12:30:00',
+                    color: 'orange'
+                  },
+                  {
+                    title: 'Lunch',
+                    start: '2021-02-12T12:00:00',
+                    color: 'green'
+                  },
+                  {
+                    title: 'Meeting',
+                    start: '2021-02-12T14:30:00',
+                    color: 'yellow'
+                    
+                  },
+                  {
+                    title: 'Birthday Party',
+                    start: '2021-02-13T07:00:00'
+                  },
+                  {
+                    title: 'Click for Google',
+                    url: 'http://google.com/',
+                    start: '2021-02-28'
+                  }
+            ]
         };
     } 
 
@@ -35,6 +89,8 @@ class Calendar extends React.Component {
                                     <FullCalendar
                                         plugins={[ dayGridPlugin ]}
                                         initialView="dayGridMonth"
+                                        weekends={false}
+                                        events={this.state.events}
                                     />
                                </div>
                             </div>
